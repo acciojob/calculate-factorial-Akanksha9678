39ba86769867e5e01168b85fbbd0718b02a01ff3
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-15 14:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let input=parseInt(prompt("enter number : "))
+
+let fact=1;
+for(let i=input; i>0;i--){
+	fact=fact*i;
+}
+alert(`The factorial of ${input} is ${fact}`)
